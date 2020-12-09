281cda7a1b375d15f2c159f654b5fc1cf04badf7
jzhowie <jzhou20@stuy.edu>
2020-12-09 14:46:52 -0500

    Initialized NoNullArrayList class with constructors + override methods

---
+++ NoNullArrayList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class NoNullArrayList<T> extends ArrayList<T> {
+
+public NoNullArrayList () {
+	super();
+}
+
+public NoNullArrayList (int capacity) {
+	super(capacity);
+}
+
+public boolean add (T element) {
+	if (element == null) {
+		throw new IllegalArgumentException("NoNullArrayList does not accept nulls.");
+	}
+	super.add(element);
+	return true;
+}
+
+public void add (int index, T element) {
+	if (element == null) {
+		throw new IllegalArgumentException("NoNullArrayList does not accept nulls.");
+	}
+	super.add(index, element);
+	return;
+}
+
+public T set (int index, T element) {
+	if (element == null) {
+		throw new IllegalArgumentException("NoNullArrayList does not accept nulls.");
+	}
+	super.set(index, element);
+	return element;
+}
+}
