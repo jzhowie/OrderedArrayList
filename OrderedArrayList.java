15726e0aedc88b33e4197559f7a48afa2ba29f91
jzhowie <jzhou20@stuy.edu>
2020-12-09 14:50:34 -0500

    Initialized OrderedArrayList with constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,11 @@
+//TODO: sort private method, add + set methods
+public class OrderedArrayList<T extends Comparable<T> > extends NoNullArrayList<T> {
+
+public OrderedArrayList () {
+	super();
+}
+
+public OrderedArrayList (int capacity) {
+	super(capacity);
+}
+}
